681f5717fe8a7a0d4f840baa2f63d1fb08f5360b
kevadesu <AnonymousM@cirno-panzer>
2024-09-30 20:58:06 +0200

    Update installer

--- installer.sh
+++ installer.sh
@@ -7,11 +7,13 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "[1/3] Installing themes..."
+echo "[1/4] Installing themes..."
 cp $SCRIPT_DIR/usr/share/themes/* /usr/share/themes/ -r
-echo "[2/3] Installing icons..."
+echo "[2/4] Installing icons and cursors..."
 cp $SCRIPT_DIR/usr/share/icons/* /usr/share/icons/ -r
-echo "[3/3] Installing sounds..."
+echo "[3/4] Installing sounds..."
 cp $SCRIPT_DIR/usr/share/sounds/* /usr/share/sounds/ -r
+echo "[4/4] Installing backgrounds..."
+cp $SCRIPT_DIR/usr/share/backgrounds/* /usr/share/backgrounds/ -r
 echo "Done!"
 exit
