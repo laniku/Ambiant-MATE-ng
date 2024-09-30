4062130e6cc6217e52843598870926649c8f3f76
LatteMangoFox <lattemangofox@gmail.com>
2024-09-30 22:14:29 +0300

    Startup sound addition

--- installer.sh
+++ installer.sh
@@ -7,13 +7,15 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "[1/4] Installing themes..."
+echo "[1/5] Installing themes..."
 cp $SCRIPT_DIR/usr/share/themes/* /usr/share/themes/ -r
-echo "[2/4] Installing icons and cursors..."
+echo "[2/5] Installing icons and cursors..."
 cp $SCRIPT_DIR/usr/share/icons/* /usr/share/icons/ -r
-echo "[3/4] Installing sounds..."
+echo "[3/5] Installing sounds..."
 cp $SCRIPT_DIR/usr/share/sounds/* /usr/share/sounds/ -r
-echo "[4/4] Installing backgrounds..."
+echo "[4/5] Installing backgrounds..."
 cp $SCRIPT_DIR/usr/share/backgrounds/* /usr/share/backgrounds/ -r
+echo "[5/5] Installing startup sound..."
+cp $SCRIPT_DIR/etc/xdg/autostart/* /etc/xdg/autostart/ -r
 echo "Done!"
 exit
